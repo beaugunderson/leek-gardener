c3b70fa891490aa9c4c7836000c52cfb69856df4
Beau Gunderson <beau@beaugunderson.com>
2023-06-05 12:56:36 -0700

    add daily fights script

---
+++ daily-fights.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./leek-gardener.js --type team --fights 20
+# ./leek-gardener.js --type solo --leek 1 --fights 50
+./leek-gardener.js --type farmer --fights 50
+./leek-gardener.js --type register
