4889913447a91317e07f714e19117d5b0689e2d2
Beau Gunderson <beau@beaugunderson.com>
2023-07-17 12:48:01 -0700

    use known node version

--- daily-fights.sh
+++ daily-fights.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./leek-gardener.js --type team --fights 20
-# ./leek-gardener.js --type solo --leek 1 --fights 50
-./leek-gardener.js --type farmer --fights 50
-./leek-gardener.js --type register
+alias node=/root/.nvm/versions/node/v16.15.0/bin/node
+
+node ./leek-gardener.js --type farmer --fights 50
+node ./leek-gardener.js --type register
+node ./leek-gardener.js --type team --fights 20
+
+# node ./leek-gardener.js --type solo --leek 1 --fights 50
