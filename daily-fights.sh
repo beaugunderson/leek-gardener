3cd3e44fb15b3e3962feca683713479298402d56
Beau Gunderson <beau@beaugunderson.com>
2023-12-08 19:58:25 -0800

    add node path

--- daily-fights.sh
+++ daily-fights.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-alias node=/root/.nvm/versions/node/v16.15.0/bin/node
-
-node ./leek-gardener.js --type farmer --fights 50
-node ./leek-gardener.js --type register
-node ./leek-gardener.js --type team --fights 20
+/root/.nvm/versions/node/v16.15.0/bin/node ./leek-gardener.js --type register
+/root/.nvm/versions/node/v16.15.0/bin/node ./leek-gardener.js --type team --fights 20
+/root/.nvm/versions/node/v16.15.0/bin/node ./leek-gardener.js --type farmer --fights 50
 
 # node ./leek-gardener.js --type solo --leek 1 --fights 50
