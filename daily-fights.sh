7a1fe140e9a5391a2b3d11bd61620d5c6fe8eaa1
Beau Gunderson <beau@beaugunderson.com>
2024-01-01 13:45:11 -0800

    add multiple logins

--- daily-fights.sh
+++ daily-fights.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/root/.nvm/versions/node/v16.15.0/bin/node ./leek-gardener.js --type team --fights 20
-/root/.nvm/versions/node/v16.15.0/bin/node ./leek-gardener.js --type farmer --fights 50
+/root/.nvm/versions/node/v16.15.0/bin/node ./leek-gardener.js --type team --fights 20 --login 1
+/root/.nvm/versions/node/v16.15.0/bin/node ./leek-gardener.js --type farmer --fights 50 --login 1
+
+/root/.nvm/versions/node/v16.15.0/bin/node ./leek-gardener.js --type solo --fights 50 --login 2
 
 # node ./leek-gardener.js --type solo --leek 1 --fights 50
